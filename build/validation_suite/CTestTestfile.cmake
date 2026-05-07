00d04464fb42cc312defd609fec92678c7899c11
Justin Yamamoto <justinkaiy02@gmail.com>
2026-05-07 14:08:00 -0700

    add .gitignore + remove existing

--- build/validation_suite/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/justiny/ros2_validation_suite/src/validation_suite
-# Build directory: /home/justiny/ros2_validation_suite/build/validation_suite
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
