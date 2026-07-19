385940f96be7abe5a5cf04a7c86fea8ecbc4172c
Justin Yamamoto <justinkaiy02@gmail.com>
2026-07-19 13:15:25 -0700

    Add odometry callback

--- src/validation_suite/scripts/prom_exporter.py
+++ src/validation_suite/scripts/prom_exporter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """prom_exporter.py
-ROS 2 node which uses prometheus_client to scrape data from "/imu_data" topic, and then expose
+ROS 2 node which uses prometheus_client to scrape data from topics, and then expose
 the gathered metrics via HTTP
 """
 
@@ -10,6 +10,8 @@ from rclpy.node import Node
 from rclpy.qos import QoSProfile, ReliabilityPolicy
 from sensor_msgs.msg import Imu
 from diagnostic_msgs.msg import DiagnosticStatus
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import TwistWithCovariance, PoseWithCovariance, Pose, Point, Quaternion, Vector3, Twist
 from prometheus_client import start_http_server, Gauge, Counter
 
 class PromExporter(Node):
@@ -17,6 +19,7 @@ class PromExporter(Node):
         super().__init__("prom_exporter")
         qos = QoSProfile(depth=10, reliability=ReliabilityPolicy.RELIABLE)
         self.create_subscription(Imu, "imu_data", self.callback, qos)
+        self.create_subscription(Odometry, "odometry_data", self.odometry_callback, qos)
         self.create_subscription(DiagnosticStatus, "imu_diag", self.status_callback, qos)
 
         self.statues = Counter('sensor_status_total', 'Counter for each status',
@@ -34,6 +37,14 @@ class PromExporter(Node):
         self.linear_acc_z = Gauge('imu_linear_acceleration_z', 'IMU linear acceleration z')
 
         self.timestamp = Gauge('timestamp', 'Timstamp')
+
+        self.pose_x = Gauge('odometry_pose_x', 'Odometry Pose X')
+        self.pose_y = Gauge('odometry_pose_y', 'Odometry Pose Y')
+        self.pose_z = Gauge('odometry_pose_z', 'Odometry Pose Z')
+
+        self.odo_orientation_x = Gauge('odometry_orientation_x', 'Odometry Orientation X')
+        self.odo_orientation_y = Gauge('odometry_orientation_y', 'Odometry Orientation Y')
+        self.odo_orientation_z = Gauge('odometry_orientation_z', 'Odometry Orientation Z')
 
     def callback(self, msg):
         """Set PromExporter node values to received metrics from topic msg
@@ -54,6 +65,21 @@ class PromExporter(Node):
 
         self.timestamp.set(time.time())
 
+    def odometry_callback(self, msg):
+        """Set PromExporter node values to received metrics from topic msg
+
+        Args: msg (nav_msgs.msg - Odometry): The given Odometry message
+        """
+        pose = msg.pose
+
+        self.pose_x = msg.pose.point.x
+        self.pose_y = msg.pose.point.y
+        self.pose_z = msg.pose.point.z
+
+        self.odo_orientation_x = msg.orientation.x
+        self.odo_orientation_y = msg.orientation.y
+        self.odo_orientation_z = msg.orientation.z
+        
     def status_callback(self, msg):
         """Set PromExporter node values to received status value from topic msg
 
