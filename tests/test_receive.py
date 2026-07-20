2b56bc01bd74508ee7c651cd3a67d4f3eea355e2
Justin Yamamoto <justinkaiy02@gmail.com>
2026-07-20 14:16:15 -0700

    Add test_receive.py

---
+++ tests/test_receive.py
@@ -0,0 +1,59 @@
+import pytest
+import math
+import transforms3d
+import time
+from rclpy.time import Time
+from rclpy.clock import ClockType
+from sensor_msgs.msg import Imu, BatteryState
+from nav_msgs.msg import Odometry
+
+class TestReceive:
+
+    def test_receive_imu_message(self, node_list_node):
+        '''
+        Test function to validate IMU messages are able to be received
+        '''
+        received = False
+
+        def imu_callback(msg):
+            nonlocal received
+            received = True
+
+        node_list_node.create_subscription(Imu, "/imu_data", imu_callback, 10)
+
+        time.sleep(3)
+
+        assert received
+
+    def test_receive_battery_message(self, node_list_node):
+        '''
+        Test function to validate BatteryState messages are able to be received
+        '''
+        received = False
+
+        def battery_callback(msg):
+            nonlocal received
+            received = True
+
+        node_list_node.create_subscription(BatteryState, "/battery_data", battery_callback, 10)
+
+        time.sleep(3)
+
+        assert received
+
+    def test_receive_batter_message(self, node_list_node):
+        '''
+        Test function to validate Odometry messages are able to be received
+        '''
+        received = False
+
+        def odometry_callback(msg):
+            nonlocal received
+            received = True
+
+        node_list_node.create_subscription(Odometry, "/odometry_data", odometry_callback, 10)
+
+        time.sleep(3)
+
+        assert received
+       
