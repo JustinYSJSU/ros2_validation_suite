d7ecbb18f052ebd324fcd131a4c654b93d69cc60
Justin Yamamoto <justinkaiy02@gmail.com>
2026-07-20 19:57:49 -0700

    Add opentelemtry export, update requirements.txt

--- junit_to_open.py
+++ junit_to_open.py
@@ -1,5 +1,15 @@
 import sys
+import os
 import xml.etree.ElementTree as ET
+
+from opentelemetry import metrics
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import (
+    PeriodicExportingMetricReader
+)
+from opentelemetry.exporter.otlp.proto.http.metric_exporter import (
+    OTLPMetricExporter
+)
 
 report_path = sys.argv[1]
 
@@ -19,11 +29,64 @@ tests = int(suite.attrib.get("tests"))
 time = float(suite.attrib.get("time"))
 passed = tests - errors - failures- skipped
 
-"""
-print(f"Tests: {tests}")
-print(f"Skipped: {skipped}")
-print(f"Error: {errors}")
-print(f"Failed: {failures}")
-print(f"Passed: {passed}")
-print(f"Time (s): {time}")
-"""
+exporter = OTLPMetricExporter(
+    endpoint=os.environ["OTEL_EXPORTER_OTLP_ENDPOINT"],
+    headers={
+        "Authorization": os.environ["OTEL_EXPORTER_OTLP_HEADERS"]
+    },
+)
+
+reader = PeriodicExportingMetricReader(
+    exporter
+)
+
+provider = MeterProvider(
+    metric_readers=[reader]
+)
+
+metrics.set_meter_provider(provider)
+meter = metrics.get_meter("pytest")
+
+test_counter = meter.create_counter("pytest.tests", description="Number of tests by result")
+test_counter.add(
+    passed,
+    {
+        "status": "passed"
+    }
+)
+
+test_counter.add(
+    failures,
+    {
+        "status": "failed"
+    }
+)
+
+test_counter.add(
+    errors,
+    {
+        "status": "errors"
+    }
+)
+
+test_counter.add(
+    skipped,
+    {
+        "status": "skipped"
+    }
+)
+
+attributes = {
+    "repository": os.getenv("GITHUB_REPOSITORY"),
+    "branch": os.getenv("GITHUB_REF_NAME"),
+    "commit": os.getenv("GITHUB_SHA", "")[:7],
+    "workflow": os.getenv("GITHUB_WORKFLOW"),
+}
+
+test_counter.add(
+    passed,
+    {
+        **attributes,
+        "status": "passed"
+    }
+)
